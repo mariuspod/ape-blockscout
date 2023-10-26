2219854ced1c7e30ae87824d6597ed99da53daf8
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2023-10-25 19:32:44 -0500

    chore: first commit

---
+++ tests/test_blockscout.py
@@ -0,0 +1,192 @@
+from typing import Callable
+
+import pytest
+
+from ape_blockscout import NETWORKS
+from ape_blockscout.exceptions import BlockscoutTooManyRequestsError
+
+# A map of each mock response to its contract name for testing `get_contract_type()`.
+EXPECTED_CONTRACT_NAME_MAP = {
+    "get_contract_response": "BoredApeYachtClub",
+    "get_proxy_contract_response": "MIM-UST-f",
+    "get_vyper_contract_response": "yvDAI",
+}
+TRANSACTION = "0x0da22730986e96aaaf5cedd5082fea9fd82269e41b0ee020d966aa9de491d2e6"
+PUBLISH_GUID = "123"
+
+# Every supported ecosystem / network combo as `[("ecosystem", "network") ... ]`
+ecosystems_and_networks = [
+    p
+    for plist in [
+        [(e, n) for n in nets] + [(e, f"{n}-fork") for n in nets] for e, nets in NETWORKS.items()
+    ]
+    for p in plist
+]
+base_url_test = pytest.mark.parametrize(
+    "ecosystem,network,url",
+    [
+        # Base
+        ("base", "mainnet", "base.blockscout.com"),
+        ("base", "mainnet-fork", "base.blockscout.com"),
+        # Ethereum
+        ("ethereum", "mainnet", "eth.blockscout.com"),
+        ("ethereum", "mainnet-fork", "eth.blockscout.com"),
+        ("ethereum", "goerli", "eth-goerli.blockscout.com"),
+        ("ethereum", "goerli-fork", "eth-goerli.blockscout.com"),
+        ("ethereum", "sepolia", "eth-sepolia.blockscout.com"),
+        ("ethereum", "sepolia-fork", "eth-sepolia.blockscout.com"),
+        # Gnosis
+        ("gnosis", "mainnet", "gnosis.blockscout.com"),
+        ("gnosis", "mainnet-fork", "gnosis.blockscout.com"),
+        ("gnosis", "chiado", "gnosis-chiado.blockscout.com"),
+        ("gnosis", "chiado-fork", "gnosis-chiado.blockscout.com"),
+        # Optimism
+        ("optimism", "mainnet", "optimism.blockscout.com"),
+        ("optimism", "mainnet-fork", "optimism.blockscout.com"),
+        ("optimism", "goerli", "optimism-goerli.blockscout.com"),
+        ("optimism", "goerli-fork", "optimism-goerli.blockscout.com"),
+        ("optimism", "sepolia", "optimism-sepolia.blockscout.com"),
+        ("optimism", "sepolia-fork", "optimism-sepolia.blockscout.com"),
+        # Polygon
+        ("polygon", "mainnet", "polygon.blockscout.com"),
+        ("polygon", "mainnet-fork", "polygon.blockscout.com"),
+    ],
+)
+
+
+@pytest.fixture
+def verification_tester_cls():
+    class VerificationTester:
+        counter = 0
+
+        def __init__(self, action_when_found: Callable, threshold: int = 2):
+            self.action_when_found = action_when_found
+            self.threshold = threshold
+
+        def sim(self):
+            # Simulate the contract type waiting in the queue until successful verification
+            if self.counter == self.threshold:
+                return self.action_when_found()
+
+            self.counter += 1
+            return "Pending in the queue"
+
+    return VerificationTester
+
+
+@pytest.fixture
+def setup_verification_test(
+    mock_backend, verification_params, verification_tester_cls, address_to_verify
+):
+    def setup(found_handler: Callable, threshold: int = 2):
+        mock_backend.setup_mock_account_transactions_response(address=address_to_verify)
+        mock_backend.add_handler("POST", "contract", verification_params, return_value=PUBLISH_GUID)
+        verification_tester = verification_tester_cls(found_handler, threshold=threshold)
+        mock_backend.add_handler(
+            "GET",
+            "contract",
+            {"guid": PUBLISH_GUID},
+            side_effect=verification_tester.sim,
+        )
+        return verification_tester
+
+    return setup
+
+
+@pytest.fixture
+def setup_verification_test_with_ctor_args(
+    mock_backend,
+    verification_params_with_ctor_args,
+    verification_tester_cls,
+    address_to_verify_with_ctor_args,
+):
+    def setup(found_handler: Callable, threshold: int = 2):
+        mock_backend.setup_mock_account_transactions_with_ctor_args_response(
+            address=address_to_verify_with_ctor_args
+        )
+        mock_backend.add_handler(
+            "POST", "contract", verification_params_with_ctor_args, return_value=PUBLISH_GUID
+        )
+        verification_tester = verification_tester_cls(found_handler, threshold=threshold)
+        mock_backend.add_handler(
+            "GET",
+            "contract",
+            {"guid": PUBLISH_GUID},
+            side_effect=verification_tester.sim,
+        )
+        return verification_tester
+
+    return setup
+
+
+@base_url_test
+def test_get_address_url(ecosystem, network, url, address, get_explorer):
+    expected = f"https://{url}/address/{address}"
+    explorer = get_explorer(ecosystem, network)
+    actual = explorer.get_address_url(address)
+    assert actual == expected
+
+
+@base_url_test
+def test_get_transaction_url(ecosystem, network, url, get_explorer):
+    expected = f"https://{url}/tx/{TRANSACTION}"
+    explorer = get_explorer(ecosystem, network)
+    actual = explorer.get_transaction_url(TRANSACTION)
+    assert actual == expected
+
+
+@pytest.mark.parametrize("ecosystem,network", ecosystems_and_networks)
+def test_get_contract_type_ecosystems_and_networks(
+    mock_backend,
+    ecosystem,
+    network,
+    get_explorer,
+):
+    # This test parametrizes getting contract types across ecosystem / network combos
+    mock_backend.set_network(ecosystem, network)
+    response = mock_backend.setup_mock_get_contract_type_response("get_contract_response")
+    explorer = get_explorer(ecosystem, network)
+    actual = explorer.get_contract_type(response.expected_address)
+    contract_type_from_lowered_address = explorer.get_contract_type(
+        response.expected_address.lower()
+    )
+    assert actual is not None
+    assert actual == contract_type_from_lowered_address
+
+    actual = actual.name
+    expected = EXPECTED_CONTRACT_NAME_MAP[response.file_name]
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "file_name", ("get_proxy_contract_response", ("get_vyper_contract_response"))
+)
+def test_get_contract_type_additional_types(mock_backend, file_name, explorer):
+    # This test parametrizes getting edge-case contract types.
+    # NOTE: Purposely not merged with test above to avoid adding a new dimension
+    #  to the parametrization.
+    response = mock_backend.setup_mock_get_contract_type_response(file_name)
+    actual = explorer.get_contract_type(response.expected_address).name
+    expected = EXPECTED_CONTRACT_NAME_MAP[response.file_name]
+    assert actual == expected
+
+
+def test_get_contract_type_with_rate_limiting(mock_backend, explorer):
+    """
+    This test ensures the rate limiting logic in the Explorer client works.
+    """
+
+    file_name = "get_vyper_contract_response"
+    setter_upper = mock_backend.setup_mock_get_contract_type_response_with_throttling
+    throttler, response = setter_upper(file_name)
+
+    # We still eventually get the response.
+    actual = explorer.get_contract_type(response.expected_address).name
+    expected = EXPECTED_CONTRACT_NAME_MAP[response.file_name]
+    assert actual == expected
+    assert throttler.counter == 2  # Prove that it actually throttled.
+
+
+def test_too_many_requests_error(no_api_key, response):
+    actual = str(BlockscoutTooManyRequestsError(response, "ethereum"))
+    assert "BLOCKSCOUT_API_KEY" in actual
