e1b5ea210eb4cec8a15a112324fe67ef25606c8b
mariuspod <14898268+mariuspod@users.noreply.github.com>
2024-04-29 18:52:37 +0200

    fix: model validations

--- ape_blockscout/config.py
+++ ape_blockscout/config.py
@@ -1,12 +1,28 @@
 from ape.api.config import PluginConfig
+from pydantic import AnyHttpUrl, model_validator
+from pydantic_settings import SettingsConfigDict
 
+class NetworkConfig(PluginConfig):
+    uri: Optional[AnyHttpUrl] = None
 
 class EcosystemConfig(PluginConfig):
+    model_config = SettingsConfigDict(extra="allow")
+
     rate_limit: int = 5  # Requests per second
     retries: int = 5  # Number of retries before giving up
 
+    @model_validator(mode="after")
+    def verify_extras(self) -> "EcosystemConfig":
+        if self.__pydantic_extra__:
+            for aname in self.__pydantic_extra__.keys():
+                self.__pydantic_extra__[aname] = NetworkConfig.model_validate(
+                    self.__pydantic_extra__[aname]
+                )
+        return self
+
 
 class BlockscoutConfig(PluginConfig):
+    model_config = SettingsConfigDict(extra="allow")
     base: EcosystemConfig = EcosystemConfig()
     ethereum: EcosystemConfig = EcosystemConfig()
     gnosis: EcosystemConfig = EcosystemConfig()
